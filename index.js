377a4c8a1484b7eba47da5bc53dfea0d098e609c
Tenuh-dev <tenuhley@gmail.com>
2026-06-19 16:25:59 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  ActivityType
+} = require("discord.js");
+
+const config = require("./config/config");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.GuildMember
+  ]
+});
+
+client.once("ready", async () => {
+  console.log(`✅ ${config.botName} Online`);
+  console.log(`🤖 Login sebagai ${client.user.tag}`);
+
+  client.user.setPresence({
+    activities: [
+      {
+        name: "Menjaga Community 🛡️",
+        type: ActivityType.Watching
+      }
+    ],
+    status: "online"
+  });
+});
+
+require("./events/messageCreate")(client);
+
+client.login(config.token);
